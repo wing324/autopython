55e115132caf68e7e73b9a57336dd701aa05c7c6
Wing <wing324@126.com>
2016-03-22 10:47:40 +0800

    Update Install_MySQL_with_rpm.py

--- Install_MySQL_with_rpm/Install_MySQL_with_rpm.py
+++ Install_MySQL_with_rpm/Install_MySQL_with_rpm.py
@@ -26,16 +26,20 @@ if os.system('groups mysql')==0:
 else:
 	os.system('groupadd mysql')
 	os.system('useradd -g mysql mysql')
+# 检查MySQL用户以及用户组是否存在，如果存在则跳过该步骤，不存在则创建MySQL用户及用户组
 # Check the exists of MySQL user and group ,if yes ,skip the reset of the script,otherwise ,execute the reset of script
 
 os.system('yum install -y perl-Module-Install.noarch libaio')
+# 安装MySQL依赖包
 # Install MySQL dependent packet
 
 rpmdir = raw_input('please input MySQL RPM packet absolute path:')
 rpmpath = rpmdir + '/MySQL-*.rpm'
 rpml = glob.glob(rpmpath)
+# 查找MySQL的RPM包
 # Find MySQL RPM packet
 
 for rpmpack in rpml:
 	os.system('rpm -ivh '+rpmpack)
+# 安装MySQL的RPM包
 # Install MySQL RPM packet
