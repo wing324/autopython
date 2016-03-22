c6e1e0ba391330fbbd4fed57e6c267ae9ea31429
Wing <wing324@126.com>
2016-03-22 10:49:55 +0800

    Update Install_Multiple_Instances_of_MySQL.py

--- Install_Multiple_Instance_of_MySQL/Install_Multiple_Instances_of_MySQL.py
+++ Install_Multiple_Instance_of_MySQL/Install_Multiple_Instances_of_MySQL.py
@@ -39,16 +39,21 @@ for p in portlist:
 	os.makedirs('%s/mysqldata%s/relaylog'% (mysql_installdir,p))
 	os.makedirs('%s/mysqldata%s/sock'% (mysql_installdir,p))
 	os.makedirs('%s/mysqldata%s/tmpdir'% (mysql_installdir,p))
+	# 创建MySQL实例目录
 	# Create the installation directory of MySQL instance
 	os.system('chown -R mysql:mysql %s' % mysql_installdir)
+	# 修改MySQL实例目录权限
 	# Change the installation directory of MySQL instance privileges
 	for line in fileinput.input('/etc/my.cnf',backup='bak',inplace=1):
 		print line.replace('[mysqld%s]' % p,'[mysqld]'),
 	fileinput.close()
+	# 修改my.cnf文件中[mysqld3306]到[mysqld]
 	# change [mysqld3306] to [mysqld] in my.cnf file 
 	os.system('mysql_install_db --user=mysql --datadir=%s/mysqldata%s/mydata/ --defaults-file=/etc/my.cnf' %(mysql_installdir,p))
+	# 安装MySQL实例
 	# install MySQL instance
 	for line in fileinput.input('/etc/my.cnf',backup='bak',inplace=1):
 		print line.replace('[mysqld]','[mysqld%s]' % p),
 	fileinput.close()
+	# 修改my.cnf文件中的[mysqld]到[mysqld3306]
 	# change [mysqld] to [mysqld3306] in my.cnf file
