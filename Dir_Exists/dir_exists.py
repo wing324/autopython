cb494af61008ece3d771681730644bb78b8c91a5
Wing <wing324@126.com>
2016-05-24 10:12:21 +0800

    Update dir_exists.py

--- Dir_Exists/dir_exists.py
+++ Dir_Exists/dir_exists.py
@@ -1,6 +1,14 @@
 #! /usr/bin/env python
 # coding:utf-8
 
+'''
+[INFORMATION]
+Directory Exists
+AUTHOR : Wing
+GitHub : https://github.com/wing324
+Email : wing324@126.com
+'''
+
 def dir_exists(directory):
 # 判断文件夹是否存在,存在则跳过,不存在则创建
 # If directory exists,skip;else,create directory
